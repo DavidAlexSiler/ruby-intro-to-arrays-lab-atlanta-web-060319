826f62dd194a66d02813934dd6498b473d08d72b
Alex Siler <davidalexsiler@gmail.com>
2019-05-22 04:28:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,6 +19,14 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  array.shift
+  array.shift()
+end
+
+def last_element_with_array_methods
+  array.pop()
+end
+
+def length_of_array(array)
+  array.length
 end
   
